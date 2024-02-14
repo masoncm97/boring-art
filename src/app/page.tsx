6f55e3c758354efb3ed4dbd713829170690be79c
Mason Mathai <masonmathai@gmail.com>
2024-02-14 16:15:00 -0500

    Using framer motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,23 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import classNames from "classnames";
-import SvgDrawing from "./svg-drawing";
-import { I, Will } from "./boring-art";
+import { BoringArt } from "./boring-art";
 import { motion } from "framer-motion";
 
-const container = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: {
-      delayChildren: 0.3,
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-export const item = {
-  hidden: { opacity: 0 },
-  visible: {
-    strokeDashoffset: 0,
-    opacity: 1,
-  },
-};
 export default function Home() {
-  const drawingIndex = useRef(0);
+  // const [svgs, setSvgs] = useState<JSX.Element[]>([<BoringArt />]);
+  const svgs = useRef<JSX.Element[]>([<BoringArt />]);
+  const drawSvg = useCallback(() => {
+    console.log("yup");
+    svgs.current.push(<BoringArt />);
+  }, []);
+
+  const intervalId = setInterval(drawSvg, 6000);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
-      <motion.div
-        className="flex w-[50vw] h-32 border border-red-500"
-        variants={container}
-        initial="hidden"
-        animate="visible"
-      >
-        <SvgDrawing
-          svgDrawing={<I className={classNames(`animate-draw-svg`)} />}
-          index={0}
-          drawingIndex={drawingIndex}
-        />
-        <SvgDrawing
-          svgDrawing={<Will className={classNames(`animate-draw-svg`)} />}
-          index={1}
-          drawingIndex={drawingIndex}
-        />
-      </motion.div>
+      {svgs.current}
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 const config = {
-  mode: "jit",
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  purge: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
